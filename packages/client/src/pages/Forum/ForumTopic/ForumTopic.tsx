511cc62175a84d5f4b8ab08523eb4ec398b6e3e1
Foxylab <rushan.shakirzianov@gmail.com>
2023-09-18 17:28:21 +0300

    fix stylelint errors

--- packages/client/src/pages/Forum/ForumTopic/ForumTopic.tsx
+++ packages/client/src/pages/Forum/ForumTopic/ForumTopic.tsx
@@ -1,8 +1,77 @@
 import React, { FC } from 'react'
 import { useParams } from 'react-router-dom'
 
-export const ForumTopic: FC = () => {
-  const { topicId } = useParams()
+import styles from './ForumTopic.module.scss'
 
-  return <div>Forum with topic id: {topicId}</div>
+import { data } from '../Forum'
+
+import { Typography } from '@alfalab/core-components/typography'
+import { Gap } from '@alfalab/core-components/gap'
+import { Comment } from '@alfalab/core-components/comment'
+import { ButtonDesktop } from '@alfalab/core-components/button/desktop'
+
+// @import '@alfalab/core-components/vars/typography.css';
+
+import { ITopic } from '../Forum'
+
+export const ForumTopic: FC = () => {
+  let topic: ITopic = {
+    id: 0,
+    date: '',
+    title: '',
+  }
+  // let comments =
+  // const IS_MOBILE = document.body.clientWidth < 450
+  const { topicId } = useParams()
+  // console.log(useParams(), topicId, data)
+  data.forEach((elem: ITopic) => {
+    if (elem.id === Number(topicId)) {
+      topic = elem
+    }
+  })
+
+  return (
+    <section className={styles.topic}>
+      {/* <div>Forum with topic id: {topicId}</div> */}
+      <Typography.TitleResponsive
+        tag="h2"
+        view="small"
+        font="system"
+        rowLimit={2}
+        // showSkeleton={showSkeleton}
+        // skeletonProps={{ width: IS_MOBILE ? ['100%', '34%'] : 420 }}
+      >
+        {topic.title}
+      </Typography.TitleResponsive>
+
+      <Gap size="s" />
+
+      <Typography.Text
+        tag="p"
+        view="primary-medium"
+        // showSkeleton={showSkeleton}
+      >
+        {topic.firstMessage}
+      </Typography.Text>
+      {/* {Object.values(topic.comments).forEach((comment) => {
+        console.log(comment)
+
+      })} */}
+      {Object.values(topic.comments as object).map(comment => (
+        <div>
+          <Comment className={styles.text}>{comment.text}</Comment>
+          <div className={styles.info}>
+            <ButtonDesktop className={styles.reply} view="ghost">
+              Ответить
+            </ButtonDesktop>
+            <div>
+              <span className={styles.author}>{comment.author}</span>
+              <span className={styles.author}>{comment.date}</span>
+            </div>
+          </div>
+          <Gap size="s" />
+        </div>
+      ))}
+    </section>
+  )
 }
