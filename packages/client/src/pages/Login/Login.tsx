8eb27c1c46a0eb36167143b61345cf67995eda02
Foxylab <rushan.shakirzianov@gmail.com>
2023-09-20 19:01:01 +0300

    Revert "Merge branch 'dev' into sek-17"
    This reverts commit eb167abe1ca10d911a2a971cf86043aaede97bdc, reversing
    changes made to 5159cb763e17581091dc63c1cdd08a4926540a28.

--- packages/client/src/pages/Login/Login.tsx
+++ packages/client/src/pages/Login/Login.tsx
@@ -1,48 +1,5 @@
-import React, { FC, useCallback, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Input } from '@alfalab/core-components/input/cssm'
-import { Button } from '@alfalab/core-components/button/cssm'
-import { PasswordInput } from '@alfalab/core-components/password-input/cssm'
-import { Gap } from '@alfalab/core-components/gap/cssm'
-import { Typography } from '@alfalab/core-components/typography/cssm'
-import { Link as LinkUI } from '@alfalab/core-components/link/cssm'
-import { AuthLayout } from '../../Layouts/AuthLayout/AuthLayout'
-import { RoutePath } from '../../constants/routes'
+import React, { FC } from 'react'
 
 export const Login: FC = () => {
-  const [passwordVisible, setPasswordVisible] = useState(false)
-
-  const changeVisibilityPassword = useCallback(() => {
-    setPasswordVisible(v => !v)
-  }, [])
-  return (
-    <AuthLayout title="Login">
-      <form>
-        <Input label="login" name="login" block size="s" />
-        <Gap size="s" />
-        <PasswordInput
-          label="password"
-          name="password"
-          block
-          size="s"
-          autoComplete="on"
-          passwordVisible={passwordVisible}
-          onPasswordVisibleChange={changeVisibilityPassword}
-        />
-        <Gap size="s" />
-        <Typography.Text view="primary-medium">
-          Don't have an account?{' '}
-          <Link to={`/${RoutePath.Registration}`}>
-            <LinkUI Component="span" underline={false} view="default">
-              Sign up
-            </LinkUI>
-          </Link>
-        </Typography.Text>
-        <Gap size="s" />
-        <Button view="accent" type="submit" block size="s">
-          Continue
-        </Button>
-      </form>
-    </AuthLayout>
-  )
+  return <div>Login</div>
 }
