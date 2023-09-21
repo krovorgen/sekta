176b8ac8b1b94f13d67ba67964f12a6672268913
levVPaname <levVPaname@yandex.ru>
2023-09-21 22:34:26 +0300

    (Root) add start game screen

--- packages/client/src/pages/Root/Root.tsx
+++ packages/client/src/pages/Root/Root.tsx
@@ -1,5 +1,40 @@
 import React, { FC } from 'react'
+import { Gap } from '@alfalab/core-components/gap/cssm'
+import { Typography } from '@alfalab/core-components/typography/cssm'
+import { Button } from '@alfalab/core-components/button/cssm'
+import { useNavigate } from 'react-router-dom'
+import { RoutePath } from '../../constants/routes'
+
+import styles from './Root.module.scss'
 
 export const Root: FC = () => {
-  return <div>Root</div>
+  const navigate = useNavigate()
+
+  return (
+    <div className={styles.root}>
+      <div className={styles.content}>
+        <Typography.Title tag="h1" color="negative" view="xlarge">
+          FIER RUNNER
+        </Typography.Title>
+        <Gap size="xs" />
+        <Typography.Title tag="h2" color="static-primary-dark" view="small">
+          Описание игры Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Etiam at sapien risus. Nulla blandit ullamcorper turpis ut rhoncus.
+          Cras gravida nibh id mauris rutrum ornare. Mauris efficitur arcu
+          ipsum, et imperdiet justo dignissim in. Duis et elementum purus, et
+          suscipit magna. Maecenas et tempus nulla. Sed rhoncus erat sit amet
+          lorem accumsan tempor
+        </Typography.Title>
+        <Gap size="s" />
+
+        <Button
+          onClick={() => navigate(RoutePath.Game)}
+          view="accent"
+          type="submit"
+          size="xs">
+          Start
+        </Button>
+      </div>
+    </div>
+  )
 }
