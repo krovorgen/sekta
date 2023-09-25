b3e817b24b91ad5e92c7433cd5bdd64805d67b0a
Foxylab <rushan.shakirzianov@gmail.com>
2023-09-25 09:05:38 +0300

    add changes from previous branch

---
+++ packages/client/src/pages/Forum/components/modal/ForumsModal.tsx
@@ -0,0 +1,70 @@
+import { FormEvent } from 'react'
+
+import { Modal } from '@alfalab/core-components/modal'
+import { Input } from '@alfalab/core-components/input'
+import { Textarea } from '@alfalab/core-components/textarea'
+import { Button } from '@alfalab/core-components/button'
+import { Gap } from '@alfalab/core-components/gap'
+
+export type ModalProps = {
+  openModal: boolean
+  handleClose: () => void
+  onSubmit: (event: FormEvent<Element>) => void
+  titleValue: string
+  setTitleValue: React.Dispatch<React.SetStateAction<string>>
+  firstMessageValue: string
+  setFirstMessageValue: React.Dispatch<React.SetStateAction<string>>
+}
+
+export const ForumsModal = ({
+  openModal,
+  handleClose,
+  onSubmit,
+  titleValue,
+  setTitleValue,
+  firstMessageValue,
+  setFirstMessageValue,
+}: ModalProps) => {
+  return (
+    <Modal open={openModal} onClose={handleClose} size={'l'}>
+      <Modal.Header
+        hasCloser={true}
+        hasBackButton={false}
+        sticky={true}
+        title={'Что вы хотите написать?'}
+      />
+
+      <Modal.Content>
+        <form onSubmit={onSubmit}>
+          <Input
+            value={titleValue}
+            block={true}
+            onChange={e => setTitleValue(e.target.value)}
+            label="Заголовок"
+          />
+          <Gap size="s" />
+          <Textarea
+            value={firstMessageValue}
+            block={true}
+            onChange={e => setFirstMessageValue(e.target.value)}
+            label="То о чем вы хотите написать"
+            size="xl"
+            minRows={3}
+            maxLength={96}
+            showCounter={true}
+          />
+        </form>
+      </Modal.Content>
+
+      <Modal.Footer sticky={false}>
+        <Button view="primary" size="s" onClick={onSubmit}>
+          Отправить
+        </Button>
+
+        <Button view="secondary" size="s" onClick={handleClose}>
+          Отмена
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
