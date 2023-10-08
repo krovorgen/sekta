64358d29f3634b13ad90d997ea0c0bc29e462e69
maksim <krovorgen@gmail.com>
2023-10-08 11:12:57 +0300

    sek-32: Добавить минимум одно Web API

---
+++ packages/client/src/components/FullscreenButton/FullscreenButton.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import screenfull from 'screenfull'
+
+import fullscreenIcon from '../../assets/icons/fullscreen.svg'
+
+import styles from './FullscreenButton.module.scss'
+
+export const FullscreenButton = () => {
+  const toggleFullscreen = () => {
+    if (screenfull.isEnabled) {
+      screenfull.toggle()
+    }
+  }
+
+  return (
+    screenfull.isEnabled && (
+      <button
+        onClick={toggleFullscreen}
+        className={styles.fullscreen}
+        type="button">
+        <img
+          className={styles.icon}
+          src={fullscreenIcon}
+          width={24}
+          height={24}
+          alt="fullscreen"
+        />
+      </button>
+    )
+  )
+}
