2bd6c89f308b846b78003d9fa7db285caf6a8e61
Pavel <b27394@gmail.com>
2023-10-11 11:49:05 +0300

    add user data to profile

--- packages/client/src/pages/Settings/Settings.tsx
+++ packages/client/src/pages/Settings/Settings.tsx
@@ -4,13 +4,16 @@ import { Notification } from '@alfalab/core-components/notification'
 import { FieldValues } from 'react-hook-form'
 
 import { withUserCheck } from '../../HOC/withUserCheck'
-import { PropsWithUser, User } from '../../types'
+import { PropsWithUser } from '../../types'
 import { Avatar } from './forms/AvatarForm'
 import { InfoForm } from './forms/InfoForm'
 import { PasswordForm } from './forms/PasswordForm'
 import { FetchMethods, fetchSekta } from '../../utils/fetch'
 import styles from './Settings.module.scss'
 import { ProfileNotification } from './types'
+import { useAppSelector } from '../../redux/store'
+import { useAppDispatch } from '../../redux/store'
+import { getUserTC } from '../../redux/features/auth/authSlice'
 
 const notificationInitState: ProfileNotification = {
   isVisible: false,
@@ -19,10 +22,13 @@ const notificationInitState: ProfileNotification = {
 }
 
 const SettingsPage: FC<PropsWithUser> = () => {
-  const [user, setUser] = useState<User>()
+  const user = useAppSelector(state => state.auth.user)
   const [notification, setNotification] = useState<ProfileNotification>(
     notificationInitState
   )
+  const dispatch = useAppDispatch()
+
+  const getUser = () => dispatch(getUserTC())
 
   const hideNotification = () => {
     setNotification(notificationInitState)
@@ -33,6 +39,7 @@ const SettingsPage: FC<PropsWithUser> = () => {
 
     if (response?.ok) {
       setNotification(prev => ({ ...prev, isVisible: true }))
+      getUser()
     } else if (response && !response.ok) {
       const result = await response.json()
       setNotification({
@@ -49,34 +56,16 @@ const SettingsPage: FC<PropsWithUser> = () => {
     }
   }
 
-  // Временный логин, нужно решить где будем хронить данные пользователя
-
-  // useEffect(() => {
-  //   const getUser = async () => {
-  //     await fetchSekta('/auth/signin', 'POST', {
-  //         login: "qqaa22",
-  //         password: "QQaa1234"
-  //       })
-
-  //     const response = await fetchSekta('/auth/user', 'GET')
-  //     const result = await response.json();
-  //     setUser(result)
-  //   }
-
-  //  getUser()
-
-  // }, [])
-
   const initials = user ? `${user.first_name[0]}${user.second_name[0]}` : 'XX'
 
   return (
     <div className={styles.pageContainer}>
       <div className={styles.content}>
         <div className={styles.contentPart}>
-          <InfoForm onSubmit={onSubmit} />
+          {user && <InfoForm onSubmit={onSubmit} user={user} />}
         </div>
         <div className={styles.contentPart}>
-          <Avatar avatar={user?.avatar} initials={initials} setUser={setUser} />
+          <Avatar avatar={user?.avatar} initials={initials} getUser={getUser} />
           <Gap size="m" />
           <PasswordForm onSubmit={onSubmit} />
         </div>
