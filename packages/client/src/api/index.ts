ffb57e1cf8c33a30210de425935920576833dc4a
Pavel <b27394@gmail.com>
2023-11-15 04:06:31 +0300

    break client prod :)

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -3,7 +3,8 @@ import ky from 'ky'
 export let apiUrl = 'http://localhost:3001/api/v2'
 
 export const isDirect =
-  process.env.SERVER_RUNNING === 'false' && !import.meta.env.PROD
+  process.env.SERVER_RUNNING === 'false' &&
+  process.env.NODE_ENV !== 'production'
 
 if (isDirect) {
   apiUrl = 'https://ya-praktikum.tech/api/v2'
