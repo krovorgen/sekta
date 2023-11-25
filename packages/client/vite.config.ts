4c613de3a18dd78d1b91ba9a51d5dc0e2a7cfe50
Pavel Bondarenko <57040691+glow27@users.noreply.github.com>
2023-11-26 00:50:16 +0300

    Update vite.config.ts

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   },
   define: {
     __SERVER_PORT__: process.env.SERVER_PORT || 3001,
+    SERVER_RUNNING: false,
   },
   plugins: [react(), EnvironmentPlugin(['SERVER_RUNNING'])],
   build: {
