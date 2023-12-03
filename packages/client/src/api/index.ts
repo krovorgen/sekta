bd839af9879279e23da97a2977a43a2855e9ffe3
Andreev D.A <d.andreev@abonentplus.ru>
2023-12-03 20:26:25 +0300

    Update index.ts

--- packages/client/src/api/index.ts
+++ packages/client/src/api/index.ts
@@ -2,13 +2,13 @@ import ky from 'ky'
 
 const isDev = () => process.env.NODE_ENV === 'development'
 
-export const apiUrl = !isDev
+export const apiUrl = !isDev()
   ? 'https://fire-runner.ya-praktikum.tech/api/'
   : 'http://localhost:3001/api/'
 
 export const proxyRoutePrefix = 'v2'
 
-export const redirect_uri = !isDev
+export const redirect_uri = !isDev()
   ? 'https://fire-runner.ya-praktikum.tech'
   : 'http://localhost:3001/signin'
 
